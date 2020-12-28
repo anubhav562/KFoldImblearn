a7d775d695c7f9d3e429ca04c7ced54a005edb12
Anubhav Chhabra <chhabra.anubhav1997@gmail.com>
2020-12-29 00:25:16 +0530

    [KFoldImblearn] - Added resampling logic for Sequential process

--- k_folds_imblearn/k_fold_imblearn.py
+++ k_folds_imblearn/k_fold_imblearn.py
@@ -21,6 +21,8 @@ class KFoldImblearn:
 
         self.__k_fold_object = self.__validate_and_instantiate_k_fold_object()
 
+        self.k_fold_dataset_list = []
+
     @property
     def k_fold_object(self):
         return self.__k_fold_object
@@ -70,7 +72,43 @@ class KFoldImblearn:
                                 f"Number of folds: {self.__k_folds}"
         return string_representation
 
+    def k_fold_fit_resample(self, X, y, processing="sequential"):
+        k_fold_indices_tuple_list = (
+            (training_indices, validation_indices) for training_indices, validation_indices in
+            self.__k_fold_object.split(X, y)
+        )
+
+        if processing == "sequential":
+            self.__fit_resample_sequentially(X, y, k_fold_indices_tuple_list)
+
+        return self.k_fold_dataset_list
+
+    def __fit_resample_sequentially(self, X, y, k_fold_indices_tuple_list):
+
+        for index, kth_index_tuple in enumerate(k_fold_indices_tuple_list):
+            training_indices = kth_index_tuple[0]
+            validation_indices = kth_index_tuple[1]
+
+            X_train = X.iloc[training_indices]
+            y_train = y.iloc[training_indices]
+
+            X_validation = X.iloc[validation_indices]
+            y_validation = y.iloc[validation_indices]
+
+            X_train_resample, y_train_resample = self.__sampling_method_object.fit_resample(X_train, y_train)
+
+            dataset_dict = {
+                f"{index+1}th_fold_resampled_train_set": (X_train_resample, y_train_resample),
+                f"{index + 1}th_fold_validation_set": (X_validation, y_validation)
+            }
+
+            self.k_fold_dataset_list.append(dataset_dict)
+
 
 if __name__ == "__main__":
-    k_fold_imblearn_object = KFoldImblearn(sampling_method="SMOTE", k_folds=3, k_fold_shuffle=True)
+    from sklearn.datasets import make_classification
+    import pandas as pd
+    X, y = make_classification(n_samples=100000, weights=(0.2, ))
+    k_fold_imblearn_object = KFoldImblearn(sampling_method="SMOTE", k_folds=10, k_fold_shuffle=True)
+    k_fold_imblearn_object.k_fold_fit_resample(pd.DataFrame(X), pd.DataFrame(y))
     print(k_fold_imblearn_object)
